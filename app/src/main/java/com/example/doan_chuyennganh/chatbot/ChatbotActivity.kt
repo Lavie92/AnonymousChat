183237d5a52b49d184f522611314b9cba55c867a
an-dt <104084358+an-dt@users.noreply.github.com>
2023-11-21 03:59:29 +0700

    done AI

---
+++ app/src/main/java/com/example/doan_chuyennganh/chatbot/ChatbotActivity.kt
@@ -0,0 +1,12 @@
+package com.example.doan_chuyennganh.chat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.doan_chuyennganh.R
+
+class ChatbotActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chatbot)
+    }
+}
