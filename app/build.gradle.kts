a136d692c2847f85a2f89fb0deb1850661d6acf5
an-dt <104084358+an-dt@users.noreply.github.com>
2023-12-17 14:27:04 +0700

    test 1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,12 +49,14 @@ android {
 }
 
 dependencies {
+    implementation ("com.mikhaellopez:circularimageview:4.3.1")
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
     implementation("com.google.firebase:firebase-ml-vision:24.1.0")
     implementation ("com.google.android.gms:play-services-vision:20.1.3")
     implementation("com.google.firebase:firebase-ml-vision-automl:18.0.6")
     implementation("com.google.firebase:firebase-ml-model-interpreter:22.0.4")
     implementation ("com.github.nipunru:nsfw-detector-android:1.0.0")
-
+    implementation ("com.airbnb.android:lottie:6.2.0")
     implementation ("com.github.ZEGOCLOUD:zego_uikit_prebuilt_call_android:+")
     implementation ("com.github.ZEGOCLOUD:zego_uikit_signaling_plugin_android:+")
     implementation ("com.github.bumptech.glide:glide:4.12.0")
@@ -83,8 +85,12 @@ dependencies {
     implementation (platform("com.google.firebase:firebase-bom:30.3.0"))
     implementation("com.google.firebase:firebase-storage-ktx:20.3.0")
     testImplementation("junit:junit:4.13.2")
+    implementation ("com.google.firebase:firebase-analytics")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation ("com.google.android.material:material:1.9.0")
+
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
 
 }
