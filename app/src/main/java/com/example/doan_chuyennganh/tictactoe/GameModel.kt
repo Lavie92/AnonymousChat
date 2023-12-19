38b0eb7d890abd17bc729adb35f442868eecd13f
an-dt <104084358+an-dt@users.noreply.github.com>
2023-12-19 19:15:22 +0700

    code chay dc

--- app/src/main/java/com/example/doan_chuyennganh/tictactoe/GameModel.kt
+++ app/src/main/java/com/example/doan_chuyennganh/tictactoe/GameModel.kt
@@ -17,3 +17,22 @@ data class GameModel(
 enum class GameStatus {
     CREATED, JOINED, INPROGRESS, FINISHED
 }
+//package np.com.bimalkafle.tictactoeonline
+//
+//import kotlin.random.Random
+//
+//data class GameModel (
+//    var gameId : String = "-1",
+//    var filledPos : MutableList<String> = mutableListOf("","","","","","","","",""),
+//    var winner : String ="",
+//    var gameStatus : GameStatus = GameStatus.CREATED,
+//    var currentPlayer : String = (arrayOf("X","O"))[Random.nextInt(2)]
+//)
+//
+//
+//enum class GameStatus{
+//    CREATED,
+//    JOINED,
+//    INPROGRESS,
+//    FINISHED
+//}
--- app/src/main/java/com/example/doan_chuyennganh/tictactoe/TictactoeActivity.kt
+++ app/src/main/java/com/example/doan_chuyennganh/tictactoe/TictactoeActivity.kt
@@ -77,3 +77,88 @@ class TictactoeActivity : AppCompatActivity() {
         startActivity(intent)
     }
 }
+//package np.com.bimalkafle.tictactoeonline
+//
+//import android.content.Intent
+//import androidx.appcompat.app.AppCompatActivity
+//import android.os.Bundle
+//import android.widget.Toast
+//import com.google.firebase.firestore.ktx.firestore
+//import com.google.firebase.firestore.ktx.toObject
+//import com.google.firebase.ktx.Firebase
+//import np.com.bimalkafle.tictactoeonline.databinding.ActivityMainBinding
+//import kotlin.random.Random
+//import kotlin.random.nextInt
+//
+//class MainActivity : AppCompatActivity() {
+//
+//    lateinit var binding : ActivityMainBinding
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        binding = ActivityMainBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+//
+//        binding.playOfflineBtn.setOnClickListener {
+//            createOfflineGame()
+//        }
+//
+//        binding.createOnlineGameBtn.setOnClickListener {
+//            createOnlineGame()
+//        }
+//
+//        binding.joinOnlineGameBtn.setOnClickListener {
+//            joinOnlineGame()
+//        }
+//
+//    }
+//
+//
+//    fun createOfflineGame(){
+//        GameData.saveGameModel(
+//            GameModel(
+//                gameStatus = GameStatus.JOINED
+//            )
+//        )
+//        startGame()
+//    }
+//
+//    fun createOnlineGame(){
+//        GameData.myID = "X"
+//        GameData.saveGameModel(
+//            GameModel(
+//                gameStatus = GameStatus.CREATED,
+//                gameId = Random.nextInt(1000..9999).toString()
+//            )
+//        )
+//        startGame()
+//    }
+//
+//    fun joinOnlineGame(){
+//        var gameId = binding.gameIdInput.text.toString()
+//        if(gameId.isEmpty()){
+//            binding.gameIdInput.setError("Please enter game ID")
+//            return
+//        }
+//        GameData.myID = "O"
+//        Firebase.firestore.collection("games")
+//            .document(gameId)
+//            .get()
+//            .addOnSuccessListener {
+//                val model = it?.toObject(GameModel::class.java)
+//                if(model==null){
+//                    binding.gameIdInput.setError("Please enter valid game ID")
+//                }else{
+//                    model.gameStatus = GameStatus.JOINED
+//                    GameData.saveGameModel(model)
+//                    startGame()
+//                }
+//            }
+//
+//    }
+//
+//    fun startGame(){
+//        startActivity(Intent(this,GameActivity::class.java))
+//    }
+//
+//}
