d744bcb8e4b643d907f83c4c47a75a0747c40c22
an-dt <104084358+an-dt@users.noreply.github.com>
2023-12-20 01:55:57 +0700

    game done

--- app/src/main/java/com/example/doan_chuyennganh/tictactoe/GameActivity.kt
+++ app/src/main/java/com/example/doan_chuyennganh/tictactoe/GameActivity.kt
@@ -3,31 +3,37 @@ package com.example.doan_chuyennganh.tictactoe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import com.example.doan_chuyennganh.databinding.ActivityGameBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.MutableData
+import com.google.firebase.database.Transaction
+import com.google.firebase.database.ValueEventListener
+import kotlin.random.Random
+
 class GameActivity : AppCompatActivity(),View.OnClickListener {
+
     lateinit var binding: ActivityGameBinding
+
     private var gameModel : GameModel? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityGameBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val gameId = intent.getStringExtra("GAME_ID")
-        val offline = intent.getBooleanExtra("OFFLINE_GAME", false)
-
-        if (offline) {
-            gameModel = GameModel()
-            setUI()
-        } else
         if (gameId != null) {
-            GameData.fetchGameModel(gameId)
+            observeGameChanges(gameId)
         } else {
             Toast.makeText(this, "Invalid Game ID", Toast.LENGTH_SHORT).show()
             finish()
         }
-
+        GameData.fetchGameModel()
 
         binding.btn0.setOnClickListener(this)
         binding.btn1.setOnClickListener(this)
@@ -47,12 +53,30 @@ class GameActivity : AppCompatActivity(),View.OnClickListener {
             gameModel = it
             setUI()
         }
-
+        binding.endGameBtn.setOnClickListener {
+            resetScores()
+            finish() // Đóng activity hiện tại và quay trở lại activity trước đó
+        }
 
 
 
     }
+    private fun observeGameChanges(gameId: String) {
+        FirebaseDatabase.getInstance().getReference("games").child(gameId)
+            .addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val game = snapshot.getValue(GameModel::class.java)
+                    game?.let {
+                        gameModel = it
+                        setUI()
+                    }
+                }
 
+                override fun onCancelled(error: DatabaseError) {
+                    Toast.makeText(this@GameActivity, "Error: ${error.message}", Toast.LENGTH_LONG).show()
+                }
+            })
+    }
     fun setUI(){
         gameModel?.apply {
             binding.btn0.text = filledPos[0]
@@ -67,39 +91,67 @@ class GameActivity : AppCompatActivity(),View.OnClickListener {
 
             binding.startGameBtn.visibility = View.VISIBLE
 
+            val currentUserIsCreator = FirebaseAuth.getInstance().currentUser?.uid == player1Id
+
             binding.gameStatusText.text = when (gameStatus) {
-                GameStatus.CREATED -> "Game Created. Waiting for another player."
-                GameStatus.JOINED -> "Game Joined. Waiting for game to start."
-                GameStatus.INPROGRESS -> {
-                    binding.startGameBtn.visibility = View.INVISIBLE
-                    if (currentUserIsPlayer1 == (currentPlayer == "X")) "Your turn"
-                    else currentPlayer + "'s turn"
-                }
-                GameStatus.FINISHED -> {
-                    if (winner.isNotEmpty()) {
-                        if (currentUserIsPlayer1 == (winner == "X")) "You won"
-                        else "Opponent won"
-                    } else "It's a draw!"
-                }
+                GameStatus.CREATED, GameStatus.JOINED -> "Waiting for game to start."
+                GameStatus.INPROGRESS -> "Game in progress."
+                GameStatus.FINISHED -> "Game finished."
             }
 
+            // Show start button if game is not yet in progress
+            binding.startGameBtn.visibility = if (gameStatus in listOf(GameStatus.CREATED, GameStatus.JOINED)) View.VISIBLE else View.INVISIBLE
+            if (gameStatus == GameStatus.FINISHED) {
+                val resultMessage = when (winner) {
+                    "X" -> "Player X wins!"
+                    "O" -> "Player O wins!"
+                    "Draw" -> "It's a draw!"
+                    else -> "Game finished!"
+                }
+                Toast.makeText(applicationContext, resultMessage, Toast.LENGTH_LONG).show()
+                binding.startGameBtn.visibility = if (gameStatus in listOf(GameStatus.CREATED, GameStatus.JOINED, GameStatus.FINISHED)) View.VISIBLE else View.INVISIBLE
+                binding.startGameBtn.setOnClickListener {
+                    if (gameModel?.gameStatus == GameStatus.FINISHED) {
+                        resetGame()
+                    } else {
+                        startGame()
+                    }
+                }
+
+            }
         }
     }
 
+    fun resetGame() {
+        gameModel = GameModel(
+            gameId = gameModel?.gameId ?: "",
+            filledPos = MutableList(9) { "" },
+            winner = "",
+            gameStatus = GameStatus.CREATED,
+            currentPlayer = if (Random.nextBoolean()) "X" else "O",
+            player1Id = FirebaseAuth.getInstance().currentUser?.uid,
+            player2Id = null,
+            waitingForOpponent = false // Hoặc true nếu bạn muốn chờ người chơi khác tham gia
+        )
+        updateGameData(gameModel!!)
+        gameModel = GameModel()
+        setUI()
+        startGame()
+    }
 
     fun startGame(){
         gameModel?.apply {
-            updateGameData(
-                GameModel(
-                    gameId = gameId,
-                    gameStatus = GameStatus.INPROGRESS
-                )
-            )
+            if (gameStatus in listOf(GameStatus.CREATED, GameStatus.JOINED)) {
+                gameStatus = GameStatus.INPROGRESS
+                updateGameData(this)
+            } else {
+                Toast.makeText(applicationContext, "Game has already started.", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
     fun updateGameData(model : GameModel){
-        GameData.saveGameModel(model)
+        FirebaseDatabase.getInstance().getReference("games").child(model.gameId).setValue(model)
     }
 
     fun checkForWinner(){
@@ -115,226 +167,151 @@ class GameActivity : AppCompatActivity(),View.OnClickListener {
         )
 
         gameModel?.apply {
-            for ( i in winningPos){
+            for (i in winningPos) {
                 //012
-                if(
+                if (
                     filledPos[i[0]] == filledPos[i[1]] &&
-                    filledPos[i[1]]== filledPos[i[2]] &&
+                    filledPos[i[1]] == filledPos[i[2]] &&
                     filledPos[i[0]].isNotEmpty()
-                ){
+                ) {
                     gameStatus = GameStatus.FINISHED
                     winner = filledPos[i[0]]
+
+                    if (gameStatus == GameStatus.FINISHED && winner != "Draw") {
+                        updatePlayerScore(winner,true)
+                    }
+                    break
                 }
             }
 
-            if( filledPos.none(){ it.isEmpty() }){
+            if (gameStatus != GameStatus.FINISHED && filledPos.none { it.isEmpty() }) {
+                // Nếu tất cả các ô đã được điền và không có người thắng
                 gameStatus = GameStatus.FINISHED
+                winner = "Draw" // Hoặc có thể để trống
             }
 
 
-            updateGameData(this)
 
+            updateGameData(this)
+            if (gameStatus == GameStatus.FINISHED) {
+                if (winner != "Draw") {
+                    val winningPlayerId =
+                        if (winner == "X") gameModel?.player1Id else gameModel?.player2Id
+                    val losingPlayerId =
+                        if (winner == "X") gameModel?.player2Id else gameModel?.player1Id
+
+                    // Cập nhật điểm số cho người thắng và người thua
+                    winningPlayerId?.let { updatePlayerScore(it, true) }
+                    losingPlayerId?.let { updatePlayerScore(it, false) }
+                }
+
+                // Theo dõi và cập nhật điểm số
+                fetchAndUpdateScores()
+            }
         }
 
+    }
 
+
+
+    fun updatePlayerScore(playerId: String, isWinner: Boolean) {
+        val playerScoreRef = FirebaseDatabase.getInstance().getReference("users/$playerId/score")
+       playerScoreRef.runTransaction(object : Transaction.Handler {
+            override fun doTransaction(mutableData: MutableData): Transaction.Result {
+                var score = mutableData.getValue(Int::class.java) ?: 0
+                if (isWinner) {
+                    score++ // Tăng điểm cho người thắng
+                }
+                mutableData.value = score
+               return Transaction.success(mutableData)
+            }
+
+            override fun onComplete(databaseError: DatabaseError?, b: Boolean, dataSnapshot: DataSnapshot?) {
+                Log.d("GameActivity", "updatePlayerScore:onComplete: $databaseError")
+            }
+        })
+    }
+
+
+    fun fetchAndUpdateScores() {
+        val player1Id = FirebaseAuth.getInstance().currentUser?.uid
+       val player2Id = gameModel?.player2Id.takeIf { it != player1Id } ?: gameModel?.player1Id
+
+        listOf(player1Id to player2Id, player2Id to player1Id).forEach { (observerId, opponentId) ->
+            FirebaseDatabase.getInstance().getReference("users/$observerId/score")
+                .addValueEventListener(object : ValueEventListener {
+                   override fun onDataChange(dataSnapshot: DataSnapshot) {
+                        val observerScore = dataSnapshot.getValue(Int::class.java) ?: 0
+                        FirebaseDatabase.getInstance().getReference("users/$opponentId/score")
+                            .addListenerForSingleValueEvent(object : ValueEventListener {
+                                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                                    val opponentScore = dataSnapshot.getValue(Int::class.java) ?: 0
+                                    updateScoreDisplay(observerScore, opponentScore)
+                                }
+
+                                override fun onCancelled(databaseError: DatabaseError) {
+                                    // Xử lý lỗi
+                               }
+                            })
+                    }
+
+                    override fun onCancelled(databaseError: DatabaseError) {
+                        // Xử lý lỗi
+                    }
+                })
+        }
+    }
+
+
+
+    fun updateScoreDisplay(playerScore: Int, opponentScore: Int) {
+        binding.scoreText.text = "Score: $playerScore - $opponentScore"
+    }
+
+    fun resetScores() {
+        // Đặt lại điểm số của cả hai người chơi về 0
+        val player1Id = gameModel?.player1Id
+        val player2Id = gameModel?.player2Id
+
+        listOf(player1Id, player2Id).forEach { playerId ->
+            playerId?.let {
+                FirebaseDatabase.getInstance().getReference("users/$it/score")
+                    .setValue(0)
+            }
+        }
     }
 
     override fun onClick(v: View?) {
         gameModel?.apply {
-            if(gameStatus != GameStatus.INPROGRESS){
-                Toast.makeText(applicationContext,"Game not started",Toast.LENGTH_SHORT).show()
+            if (gameStatus != GameStatus.INPROGRESS) {
+                Toast.makeText(applicationContext, "Game not started", Toast.LENGTH_SHORT).show()
                 return
             }
 
-            //game is in progress
-            if(!currentUserIsPlayer1 == (currentPlayer == "X")) {
-                Toast.makeText(applicationContext, "Not your turn", Toast.LENGTH_SHORT).show()
-                return
-            }
+            val clickedPos = (v?.tag as String).toInt()
+            if (filledPos[clickedPos].isEmpty()) {
+                val currentUserIsX = FirebaseAuth.getInstance().currentUser?.uid == player1Id
+                if ((currentPlayer == "X" && currentUserIsX) || (currentPlayer == "O" && !currentUserIsX)) {
+                    filledPos[clickedPos] = currentPlayer
+                    currentPlayer = if (currentPlayer == "X") "O" else "X"
+                    checkForWinner()
+                    updateGameData(this)
 
-            val clickedPos =(v?.tag  as String).toInt()
-            if(filledPos[clickedPos].isEmpty()){
-                filledPos[clickedPos] = currentPlayer
-                currentPlayer = if(currentPlayer=="X") "O" else "X"
-                checkForWinner()
-                updateGameData(this)
+                } else {
+                    Toast.makeText(applicationContext, "Not your turn", Toast.LENGTH_SHORT).show()
+                }
             }
-
         }
     }
 }
-//package np.com.bimalkafle.tictactoeonline
-//
-//import androidx.appcompat.app.AppCompatActivity
-//import android.os.Bundle
-//import android.view.View
-//import android.widget.Toast
-//import np.com.bimalkafle.tictactoeonline.databinding.ActivityGameBinding
-//
-//class GameActivity : AppCompatActivity(),View.OnClickListener {
-//
-//    lateinit var binding: ActivityGameBinding
-//
-//    private var gameModel : GameModel? = null
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        binding = ActivityGameBinding.inflate(layoutInflater)
-//        setContentView(binding.root)
-//
-//        GameData.fetchGameModel()
-//
-//        binding.btn0.setOnClickListener(this)
-//        binding.btn1.setOnClickListener(this)
-//        binding.btn2.setOnClickListener(this)
-//        binding.btn3.setOnClickListener(this)
-//        binding.btn4.setOnClickListener(this)
-//        binding.btn5.setOnClickListener(this)
-//        binding.btn6.setOnClickListener(this)
-//        binding.btn7.setOnClickListener(this)
-//        binding.btn8.setOnClickListener(this)
-//
-//        binding.startGameBtn.setOnClickListener {
-//            startGame()
-//        }
-//
-//        GameData.gameModel.observe(this){
-//            gameModel = it
-//            setUI()
-//        }
-//
-//
-//
-//
-//    }
-//
-//    fun setUI(){
-//        gameModel?.apply {
-//            binding.btn0.text = filledPos[0]
-//            binding.btn1.text = filledPos[1]
-//            binding.btn2.text = filledPos[2]
-//            binding.btn3.text = filledPos[3]
-//            binding.btn4.text = filledPos[4]
-//            binding.btn5.text = filledPos[5]
-//            binding.btn6.text = filledPos[6]
-//            binding.btn7.text = filledPos[7]
-//            binding.btn8.text = filledPos[8]
-//
-//            binding.startGameBtn.visibility = View.VISIBLE
-//
-//            binding.gameStatusText.text =
-//                when(gameStatus){
-//                    GameStatus.CREATED -> {
-//                        binding.startGameBtn.visibility = View.INVISIBLE
-//                        "Game ID :"+ gameId
-//                    }
-//                    GameStatus.JOINED ->{
-//                        "Click on start game"
-//                    }
-//                    GameStatus.INPROGRESS ->{
-//                        binding.startGameBtn.visibility = View.INVISIBLE
-//                        when(GameData.myID){
-//                            currentPlayer -> "Your turn"
-//                            else ->  currentPlayer + " turn"
-//                        }
-//
-//                    }
-//                    GameStatus.FINISHED ->{
-//                        if(winner.isNotEmpty()) {
-//                            when(GameData.myID){
-//                                winner -> "You won"
-//                                else ->   winner + " Won"
-//                            }
-//
-//                        }
-//                        else "DRAW"
-//                    }
-//                }
-//
-//        }
-//    }
-//
-//
-//    fun startGame(){
-//        gameModel?.apply {
-//            updateGameData(
-//                GameModel(
-//                    gameId = gameId,
-//                    gameStatus = GameStatus.INPROGRESS
-//                )
-//            )
-//        }
-//    }
-//
-//    fun updateGameData(model : GameModel){
-//        GameData.saveGameModel(model)
-//    }
-//
-//    fun checkForWinner(){
-//        val winningPos = arrayOf(
-//            intArrayOf(0,1,2),
-//            intArrayOf(3,4,5),
-//            intArrayOf(6,7,8),
-//            intArrayOf(0,3,6),
-//            intArrayOf(1,4,7),
-//            intArrayOf(2,5,8),
-//            intArrayOf(0,4,8),
-//            intArrayOf(2,4,6),
-//        )
-//
-//        gameModel?.apply {
-//            for ( i in winningPos){
-//                //012
-//                if(
-//                    filledPos[i[0]] == filledPos[i[1]] &&
-//                    filledPos[i[1]]== filledPos[i[2]] &&
-//                    filledPos[i[0]].isNotEmpty()
-//                ){
-//                    gameStatus = GameStatus.FINISHED
-//                    winner = filledPos[i[0]]
-//                }
-//            }
-//
-//            if( filledPos.none(){ it.isEmpty() }){
-//                gameStatus = GameStatus.FINISHED
-//            }
-//
-//
-//            updateGameData(this)
-//
-//        }
-//
-//
-//    }
-//
-//    override fun onClick(v: View?) {
-//        gameModel?.apply {
-//            if(gameStatus!= GameStatus.INPROGRESS){
-//                Toast.makeText(applicationContext,"Game not started",Toast.LENGTH_SHORT).show()
-//                return
-//            }
-//
-//            //game is in progress
-//            if(gameId!="-1" && currentPlayer!=GameData.myID ){
-//                Toast.makeText(applicationContext,"Not your turn",Toast.LENGTH_SHORT).show()
-//                return
-//            }
-//
-//            val clickedPos =(v?.tag  as String).toInt()
-//            if(filledPos[clickedPos].isEmpty()){
-//                filledPos[clickedPos] = currentPlayer
-//                currentPlayer = if(currentPlayer=="X") "O" else "X"
-//                checkForWinner()
-//                updateGameData(this)
-//            }
-//
-//        }
-//    }
-//}
-//
-//
-//
-//
+
+
+
+
+
+
+
+
 
 
 
--- app/src/main/java/com/example/doan_chuyennganh/tictactoe/TictactoeActivity.kt
+++ app/src/main/java/com/example/doan_chuyennganh/tictactoe/TictactoeActivity.kt
@@ -6,159 +6,119 @@ import android.os.Bundle
 import android.widget.Toast
 import com.example.doan_chuyennganh.databinding.ActivityTictactoeBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlin.random.Random
+import kotlin.random.nextInt
 
 class TictactoeActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityTictactoeBinding
-    private val firebaseDatabase = FirebaseDatabase.getInstance()
-    private val currentUserID: String? = FirebaseAuth.getInstance().currentUser?.uid
+    lateinit var binding: ActivityTictactoeBinding
+    private val database = FirebaseDatabase.getInstance().getReference("games")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityTictactoeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         binding.playOfflineBtn.setOnClickListener {
-            startGameOffline()
+            createOfflineGame()
         }
+
+        binding.createOnlineGameBtn.setOnClickListener {
+            createOnlineGame()
+        }
+
         binding.joinOnlineGameBtn.setOnClickListener {
-            findOpponentAndStartGame()
+            joinOrCreateGame()
         }
+
     }
-    private fun startGameOffline() {
-        val intent = Intent(this, GameActivity::class.java).apply {
-            putExtra("OFFLINE_GAME", true)
-        }
-        startActivity(intent)
-    }
-    private fun findOpponentAndStartGame() {
-        currentUserID?.let { userId ->
-            // Tìm kiếm trò chơi chưa hoàn thành
-            firebaseDatabase.getReference("games")
-                .orderByChild("waitingForOpponent")
-                .equalTo(true)
-                .limitToFirst(1)
-                .get()
-                .addOnSuccessListener { dataSnapshot ->
-                    if (dataSnapshot.childrenCount > 0) {
-                        // Có trò chơi đang chờ, tham gia vào trò chơi đó
-                        val game = dataSnapshot.children.first()
-                        val gameId = game.key!!
-                        updateGameSessionWithSecondPlayer(gameId, userId)
-                    } else {
-                        // Không có trò chơi nào, tạo trò chơi mới
-                        createNewGame(userId)
+    private fun joinOrCreateGame() {
+        val userId = FirebaseAuth.getInstance().currentUser?.uid
+        userId?.let { uid ->
+            database.orderByChild("waitingForOpponent").equalTo(true).limitToFirst(1)
+                .addListenerForSingleValueEvent(object : ValueEventListener {
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                        if (snapshot.exists()) {
+                            val game = snapshot.children.first()
+                            joinGame(game.key!!, uid)
+                        } else {
+                            createNewGame(uid)
+                        }
                     }
-                }
-                .addOnFailureListener {
-                    Toast.makeText(this, "Unable to find or create a game.", Toast.LENGTH_SHORT).show()
-                }
-        }?: run {
-            Toast.makeText(this, "User not logged in.", Toast.LENGTH_SHORT).show()
+
+                    override fun onCancelled(error: DatabaseError) {
+                        Toast.makeText(this@TictactoeActivity, "Error: ${error.message}", Toast.LENGTH_LONG).show()
+                    }
+                })
         }
     }
-
     private fun createNewGame(userId: String) {
-        val gameId = firebaseDatabase.getReference("games").push().key!!
-        firebaseDatabase.getReference("games/$gameId")
-            .setValue(GameModel(gameId = gameId, player1Id = userId, waitingForOpponent = true))
-        navigateToGameScreen(gameId)
+        val newGameId = database.push().key!!
+        val newGame = GameModel(gameId = newGameId, player1Id = userId)
+        database.child(newGameId).setValue(newGame)
+        navigateToGameScreen(newGameId)
     }
 
-    private fun updateGameSessionWithSecondPlayer(gameId: String, userId: String) {
-        firebaseDatabase.getReference("games/$gameId")
-            .updateChildren(mapOf("player2Id" to userId, "waitingForOpponent" to false))
+    private fun joinGame(gameId: String, userId: String) {
+        database.child(gameId).updateChildren(mapOf("player2Id" to userId, "waitingForOpponent" to false))
         navigateToGameScreen(gameId)
     }
-
     private fun navigateToGameScreen(gameId: String) {
         val intent = Intent(this, GameActivity::class.java).apply {
             putExtra("GAME_ID", gameId)
         }
         startActivity(intent)
     }
+    fun createOfflineGame(){
+        GameData.saveGameModel(
+            GameModel(
+                gameStatus = GameStatus.JOINED
+            )
+        )
+        startGame()
+    }
+
+    fun createOnlineGame(){
+        GameData.myID = "X"
+        GameData.saveGameModel(
+            GameModel(
+                gameStatus = GameStatus.CREATED,
+                gameId = Random.nextInt(1000..9999).toString()
+            )
+        )
+        startGame()
+    }
+
+    fun joinOnlineGame(){
+        var gameId = binding.gameIdInput.text.toString()
+        if(gameId.isEmpty()){
+            binding.gameIdInput.setError("Please enter game ID")
+            return
+        }
+        GameData.myID = "O"
+        Firebase.firestore.collection("games")
+            .document(gameId)
+            .get()
+            .addOnSuccessListener {
+                val model = it?.toObject(GameModel::class.java)
+                if(model==null){
+                    binding.gameIdInput.setError("Please enter valid game ID")
+                }else{
+                    model.gameStatus = GameStatus.JOINED
+                    GameData.saveGameModel(model)
+                    startGame()
+                }
+            }
+
+    }
+
+    fun startGame(){
+        startActivity(Intent(this,GameActivity::class.java))
+    }
+
 }
-//package np.com.bimalkafle.tictactoeonline
-//
-//import android.content.Intent
-//import androidx.appcompat.app.AppCompatActivity
-//import android.os.Bundle
-//import android.widget.Toast
-//import com.google.firebase.firestore.ktx.firestore
-//import com.google.firebase.firestore.ktx.toObject
-//import com.google.firebase.ktx.Firebase
-//import np.com.bimalkafle.tictactoeonline.databinding.ActivityMainBinding
-//import kotlin.random.Random
-//import kotlin.random.nextInt
-//
-//class MainActivity : AppCompatActivity() {
-//
-//    lateinit var binding : ActivityMainBinding
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        binding = ActivityMainBinding.inflate(layoutInflater)
-//        setContentView(binding.root)
-//
-//        binding.playOfflineBtn.setOnClickListener {
-//            createOfflineGame()
-//        }
-//
-//        binding.createOnlineGameBtn.setOnClickListener {
-//            createOnlineGame()
-//        }
-//
-//        binding.joinOnlineGameBtn.setOnClickListener {
-//            joinOnlineGame()
-//        }
-//
-//    }
-//
-//
-//    fun createOfflineGame(){
-//        GameData.saveGameModel(
-//            GameModel(
-//                gameStatus = GameStatus.JOINED
-//            )
-//        )
-//        startGame()
-//    }
-//
-//    fun createOnlineGame(){
-//        GameData.myID = "X"
-//        GameData.saveGameModel(
-//            GameModel(
-//                gameStatus = GameStatus.CREATED,
-//                gameId = Random.nextInt(1000..9999).toString()
-//            )
-//        )
-//        startGame()
-//    }
-//
-//    fun joinOnlineGame(){
-//        var gameId = binding.gameIdInput.text.toString()
-//        if(gameId.isEmpty()){
-//            binding.gameIdInput.setError("Please enter game ID")
-//            return
-//        }
-//        GameData.myID = "O"
-//        Firebase.firestore.collection("games")
-//            .document(gameId)
-//            .get()
-//            .addOnSuccessListener {
-//                val model = it?.toObject(GameModel::class.java)
-//                if(model==null){
-//                    binding.gameIdInput.setError("Please enter valid game ID")
-//                }else{
-//                    model.gameStatus = GameStatus.JOINED
-//                    GameData.saveGameModel(model)
-//                    startGame()
-//                }
-//            }
-//
-//    }
-//
-//    fun startGame(){
-//        startActivity(Intent(this,GameActivity::class.java))
-//    }
-//
-//}
